67adeaf2be90131a3fb36d5ee3d42c4c4d530767
Shishikyuu <richard.febres@gmail.com>
2021-04-05 12:58:46 -0400

    Add constructors for both types of operations, unary and binary

--- CalculatorLib/Models/Calculation.cs
+++ CalculatorLib/Models/Calculation.cs
@@ -1,24 +1,39 @@
-﻿namespace IS421Midterm.CalculatorLib.Models
+﻿using IS421Midterm.CalculatorLib.Interfaces;
+using System;
+
+namespace IS421Midterm.CalculatorLib.Models
 {
-    public class Calculation
+    public class Calculation : ICalculation
     {
         public double First { get; set; }
 
         public double Second { get; set; }
 
-        public string Operation { get; set; }
+        public Func<double, double, double> BinaryOperation { get; set; }
 
-        public double Result { get; private set; }
+        public Func<double, double> UnaryOperation { get; set; }
 
-        public double HasResult { get; set; }
-
-        public bool WasModified { get; set; }
-        
-        public Calculation(double first, double second, string op)
+        public Calculation(double first, double second, Func<double, double, double> op)
         {
             First = first;
             Second = second;
-            Operation = op;
+            BinaryOperation = op;
+        }
+
+        public Calculation(double first, Func<double, double> op)
+        {
+            First = first;
+            UnaryOperation = op;
+        }
+
+        public double GetBinaryResult()
+        {
+            return BinaryOperation(First, Second);
+        }
+
+        public double GetUnaryResult()
+        {
+            return UnaryOperation(First);
         }
     }
 }
