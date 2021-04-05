cc05823d54719efe4be7506773fbe3ceeba5e243
Shishikyuu <richard.febres@gmail.com>
2021-04-05 13:44:09 -0400

    Add event

---
+++ Client/ConsoleManager/Events/EnterMenuOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IS421Midterm.Client.ConsoleManager.Events
+{
+    public class EnterMenuOption
+    {
+        public void OnLog(object sender, EventArgs e)
+        {
+            Console.WriteLine("Please enter 1 of 3 options, listed below:");
+            Console.WriteLine("\t0 - Begin/Continue Calculating");
+            Console.WriteLine("\t1 - View History");
+            Console.WriteLine("\t2 - Exit");
+        }
+    }
+}
