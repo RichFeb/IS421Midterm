6141034f79c12dbb7da79a7065c6efa93572dcd3
Shishikyuu <richard.febres@gmail.com>
2021-04-05 13:43:35 -0400

    Moved to Events namespace

---
+++ Client/ConsoleManager/Events/ConsoleEvent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace IS421Midterm.Client.ConsoleManager.Events
+{
+    public class ConsoleEvent
+    {
+        public event EventHandler<ConsoleEventArgs> UserInput;
+
+        public event EventHandler LogMessage;
+
+        public void GetUserStringInput(string input)
+        {
+            OnUserInput(input);
+        }
+
+        protected virtual void OnUserInput(string input)
+        {
+            UserInput?.Invoke(this, new ConsoleEventArgs() { StringInput = input });
+        }
+
+        public void GetUserNumericInput(double input)
+        {
+            OnUserInput(input);
+        }
+
+        protected virtual void OnUserInput(double input)
+        {
+            UserInput?.Invoke(this, new ConsoleEventArgs() { NumericInput = input });
+        }
+
+        public void GetUserAction(Func<double, double, double> input)
+        {
+            OnUserInput(input);
+        }
+
+        protected virtual void OnUserInput(Func<double, double, double> input)
+        {
+            UserInput?.Invoke(this, new ConsoleEventArgs() { OperationInput = input });
+        }
+
+        public void Log()
+        {
+            OnLog();
+        }
+        protected virtual void OnLog()
+        {
+            LogMessage?.Invoke(this, EventArgs.Empty);
+
+        }
+
+
+    }
+}
---
+++ Client/ConsoleManager/Events/ConsoleEventAggregator.cs
@@ -0,0 +1,67 @@
+﻿using IS421Midterm.Client.ConsoleManager.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS421Midterm.Client.ConsoleManager.Events
+{
+    public class ConsoleEventAggregator
+    {
+        public ConsoleEvent consoleEvent = new ConsoleEvent();
+        ProgramInit programInit = new ProgramInit();
+        EnterOperation enterOperation = new EnterOperation();
+        EnterNumericInput enterNumericInput = new EnterNumericInput();
+        EnterMenuOption enterMenuOption = new EnterMenuOption();
+        DivideByZero divideByZero = new DivideByZero();
+
+        public void RegisterProgramInit()
+        {
+            consoleEvent.LogMessage += programInit.OnLog;
+        }
+
+        public void UnregisterProgramInit()
+        {
+            consoleEvent.LogMessage -= programInit.OnLog;
+        }
+
+        public void RegisterEnterOperation()
+        {
+            consoleEvent.LogMessage += enterOperation.OnLog;
+        }
+
+        public void UnregisterEnterOperation()
+        {
+            consoleEvent.LogMessage -= enterOperation.OnLog;
+        }
+
+        public void RegisterEnterNumericInput()
+        {
+            consoleEvent.LogMessage += enterNumericInput.OnLog;
+        }
+
+        public void UnregisterEnterNumericInput()
+        {
+            consoleEvent.LogMessage -= enterNumericInput.OnLog;
+        }
+
+        public void RegisterEnterMenuOption()
+        {
+            consoleEvent.LogMessage += enterMenuOption.OnLog;
+        }
+
+        public void UnregisterEnterMenuOption()
+        {
+            consoleEvent.LogMessage -= enterMenuOption.OnLog;
+        }
+
+        public void RegisterDivideByZero()
+        {
+            consoleEvent.LogMessage += divideByZero.OnLog;
+        }
+
+        public void UnregisterDivideByZero()
+        {
+            consoleEvent.LogMessage -= divideByZero.OnLog;
+        }
+    }
+}
