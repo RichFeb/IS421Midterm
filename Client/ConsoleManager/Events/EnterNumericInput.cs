dc60409dfab948839b0edaee0ecd730dfe15b5f9
Shishikyuu <richard.febres@gmail.com>
2021-04-05 13:15:56 -0400

    Add new event

---
+++ Client/ConsoleManager/Events/EnterNumericInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS421Midterm.Client.ConsoleManager.Events
+{
+    public class EnterNumericInput
+    {
+        public void OnLog(object sender, EventArgs e)
+        {
+            Console.WriteLine("Enter enter a number.");
+        } 
+    }
+}
