f79f4fbb1c10972fb011f7285228f2b32c03df1d
Shishikyuu <richard.febres@gmail.com>
2021-04-05 13:45:06 -0400

    Add ConsoleObserver

---
+++ Client/ConsoleManager/Observer/ConsoleObserver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS421Midterm.Client.ConsoleManager.Observer
+{
+    public class ConsoleObserver
+    {
+
+
+    }
+}
