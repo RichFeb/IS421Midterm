abdbdfe0a7b24caddc6885f142b41b82b2e6cc2e
Shishikyuu <richard.febres@gmail.com>
2021-04-05 13:16:04 -0400

    Add new event

---
+++ Client/ConsoleManager/Events/EnterOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS421Midterm.Client.ConsoleManager.Events
+{
+    public class EnterOperation
+    {
+        public void OnLog(object sender, EventArgs e)
+        {
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\t+ - Add");
+            Console.WriteLine("\t- - Subtract");
+            Console.WriteLine("\t* - Multiply");
+            Console.WriteLine("\t/ - Divide");
+            Console.WriteLine("\t** - Exp");
+            Console.WriteLine("\tsqrt - Square Root");
+     
+        }
+    }
+}
