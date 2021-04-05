7241ac6835029ab9ca4cb4734ac1ed315325207f
Shishikyuu <richard.febres@gmail.com>
2021-04-05 13:16:13 -0400

    Add new event

---
+++ Client/ConsoleManager/Events/ProgramInit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS421Midterm.Client.ConsoleManager.Events
+{
+    public class ProgramInit
+    {
+        public void OnLog(object sender, EventArgs e)
+        {
+            Console.WriteLine("Calculator program startup commencing. Please enter an option.");
+        }
+    }
+}
