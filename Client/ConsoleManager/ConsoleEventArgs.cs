f0c1bc76ae9011dd7ad561fb9e570063aaa03abc
Shishikyuu <richard.febres@gmail.com>
2021-04-05 13:16:48 -0400

    Add ConsoleEventArgs which extends System.EventArgs

---
+++ Client/ConsoleManager/ConsoleEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IS421Midterm.Client.ConsoleManager
+{
+    public class ConsoleEventArgs : EventArgs
+    {
+        public double NumericInput { get; set; }
+
+        public Func<double, double, double> OperationInput { get; set; }
+
+        public string StringInput { get; set; }
+
+    }
+}
