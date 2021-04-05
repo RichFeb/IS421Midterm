2eef1aba8b28411efa1770e0739b4137445619a9
Shishikyuu <richard.febres@gmail.com>
2021-04-05 18:15:12 -0400

    Add IConsoleObserver interface

---
+++ Client/Interfaces/IConsoleSubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS421Midterm.Client.Interfaces
+{
+    public interface IConsoleSubject
+    {
+        void Attach(IConsoleObserver observer);
+
+        void Detach(IConsoleObserver observer);
+
+        void Notify();
+    }
+}
