1c2de6819889b8a494db1e403a46b47ab2a4ec15
Shishikyuu <richard.febres@gmail.com>
2021-04-05 21:15:20 -0400

    Update ConsoleManager

--- Client/ConsoleManager.cs
+++ Client/ConsoleManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IS421Midterm.CalculatorLib.Models;
+using IS421Midterm.Client.Events;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +8,18 @@ namespace IS421Midterm.Client
 {
     public class ConsoleManager
     {
+
+
         public void Run()
         {
-            
+            var calculator = Calculator.Instance;
+            var aggregator = new ConsoleEventAggregator();
+
+        }
+
+        internal object GetLastInput()
+        {
+            throw new NotImplementedException();
         }
     }
 }
